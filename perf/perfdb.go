5d7d89421424ca32e939ec7b95d19dfb862adacc
flywukong <2229306838@qq.com>
2024-09-25 15:01:52 +0800

    fix

--- perf/perfdb.go
+++ perf/perfdb.go
@@ -1056,7 +1056,8 @@ func (d *DBRunner) InitSingleStorageTrie(
 }
 
 func (d *DBRunner) trySleep() {
-	time.Sleep(time.Duration(d.perfConfig.SleepTime) * time.Millisecond)
+	// time.Sleep(time.Duration(d.perfConfig.SleepTime) * time.Millisecond)
+	time.Sleep(50 * time.Millisecond)
 }
 
 func (d *DBRunner) isLargeStorageTrie(owner common.Address) bool {
